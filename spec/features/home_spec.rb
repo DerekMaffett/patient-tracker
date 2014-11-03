ad00871cdfaf40150797644b8dc9d67b9e713151
Derek Maffett <derekgmaffett@gmail.com>
2014-11-03 14:55:54 -0800

    refactor to require explicit declaration of created users

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
 describe 'sign-in' do
+  before(:each) do
+    @resident = create(:resident)
+  end
+
   it 'registered users can sign in' do
-    log_in_as :resident
+    log_in_as @resident
     expect(page).to have_content('Signed in successfully')
   end
 end
--- spec/support/authorization_helpers.rb
+++ spec/support/authorization_helpers.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 def log_in_as(user)
-  user = create(user)
   visit '/'
   click_on 'sign in'
   fill_in 'Email', with: user.email
