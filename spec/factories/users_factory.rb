98f175fa8da7a37ac623cb0dcc6f0870781a334c
Derek Maffett <derekgmaffett@gmail.com>
2014-11-03 23:35:32 -0800

    apply Faker to user factories

--- spec/factories/users_factory.rb
+++ spec/factories/users_factory.rb
@@ -1,23 +1,23 @@
 FactoryGirl.define do
   factory :resident, class: User do
-    name 'Gregory House'
+    name { "#{first_name} #{last_name}" }
     role 'Resident'
-    email 'gregoryhouse@hospital.com'
+    email { Faker::Internet.email }
     password 'resident'
     password_confirmation 'resident'
-    first_name 'Gregory'
-    last_name 'House'
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     confirmed_at Time.now
   end
 
   factory :admin, class: User do
-    name 'Lisa Cuddy'
+    name { "#{first_name} #{last_name}" }
     role 'Admin'
-    email 'lisacuddy@hospital.com'
+    email { Faker::Internet.email }
     password 'admin'
     password_confirmation 'admin'
-    first_name 'Lisa'
-    last_name 'Cuddy'
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     confirmed_at Time.now
   end
 end
