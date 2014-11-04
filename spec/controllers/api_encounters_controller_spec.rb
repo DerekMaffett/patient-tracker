59d99f977c4c315422465d237a807afaa8ad39d8
Derek Maffett <derekgmaffett@gmail.com>
2014-11-03 21:12:37 -0800

    pull apart tests for clarity

--- spec/controllers/api_encounters_controller_spec.rb
+++ spec/controllers/api_encounters_controller_spec.rb
@@ -16,19 +16,34 @@ RSpec.describe Api::V1::EncountersController, :type => :controller do
     describe 'when a resident has multiple encounters' do
       before(:each) do
         2.times { create(:encounter, user_id: @resident.id) }
-      end
-
-      it 'shows encounters of a resident' do
         log_in_as @resident
         get :index
+      end
+
+      it 'should have a 200 status' do
         expect(response).to have_http_status(200)
+      end
+
+      it 'should have a size equal to the number of records' do
         expect(json(response)[:encounters].size).to eq 2
+      end
+
+      it 'should list the date encountered' do
         expect(json(response)[:encounters][0][:encountered_on].to_date)
           .to be_between Date.today - 30.days, Date.today
+      end
+
+      it 'should list a custom submitted_on attr as the submission time' do
         expect(json(response)[:encounters][0][:submitted_on].to_date)
           .to eq Date.today
+      end
+
+      it "should give the associated user's name" do
         expect(json(response)[:encounters][0][:user][:name])
           .to eq @resident.name
+      end
+
+      it 'should not return parameters such as password' do
         expect(json(response)[:encounters][0][:user][:encrypted_password])
           .to be_nil
       end
