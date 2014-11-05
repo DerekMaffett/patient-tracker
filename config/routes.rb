29fb7bc2bb459c90f2619a104ce25890a2eda22c
Derek Maffett <derekgmaffett@gmail.com>
2014-11-05 13:01:02 -0800

    add nav directive

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'encounters/summary' => 'encounters#summary', as: :summary
-  # resources :encounters
+  get 'encounters/summary' => 'encounters#summary', as: :summary
+  resources :encounters
 
   namespace :api do
     namespace :v1 do
