0a40b664e89b2bdc7378b9c328d29a7106f15c52
Derek Maffett <derekgmaffett@gmail.com>
2014-11-06 21:57:27 -0800

    add serializer for groups

--- spec/controllers/api/groups_index_spec.rb
+++ spec/controllers/api/groups_index_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Api::V1::GroupsController, type: :controller do
         expect { get :index }.to raise_error Pundit::NotAuthorizedError
       end
     end
+
     context 'when the user is a resident' do
       before(:each) do
         @resident = create(:resident)
@@ -20,7 +21,28 @@ RSpec.describe Api::V1::GroupsController, type: :controller do
           expect(json(response)[:groups]).to be_empty
         end
       end
+
+      context 'when there are groups' do
+        before(:each) do
+          3.times { create(:group) }
+          log_in_as @resident
+          get :index
+        end
+
+        it 'should return http status 200' do
+          expect(response).to have_http_status 200
+        end
+
+        it 'should return all groups' do
+          expect(json(response)[:groups].size).to eq Group.count
+        end
+
+        it 'should include group names' do
+          Group.all.each do |group|
+            expect(json(response)[:groups][0][:name]).to eq Group.first.name
+          end
+        end
+      end
     end
-    context
   end
 end
