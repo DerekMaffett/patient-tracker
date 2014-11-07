5a3b84d27deec2468cbe1de73b189cc0afb9009c
Nicholas Manos <manos.nick@gmail.com>
2014-11-06 18:06:32 -0800

    user avatar fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  mount_uploader :avatar_image, AvatarUploader
+  # mount_uploader :avatar_image, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
