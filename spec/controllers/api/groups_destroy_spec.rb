3be84897fe071bd485df59ea943f06dbafa78d53
Derek Maffett <derekgmaffett@gmail.com>
2014-11-07 00:14:44 -0800

    signed in users can not delete groups

--- spec/controllers/api/groups_destroy_spec.rb
+++ spec/controllers/api/groups_destroy_spec.rb
@@ -4,9 +4,39 @@ RSpec.describe Api::V1::GroupsController, type: :controller do
   describe 'DELETE #destroy' do
     before(:each) do
       @group = create(:group)
+      @resident = create(:resident)
     end
 
     context 'a user is not signed in' do
+      it 'should raise an error' do
+        expect { delete :destroy, id: @group }.to raise_error Pundit::NotAuthorizedError
+      end
+
+      it 'should not be deleted' do
+        expect(Group.count).to eq 1
+      end
+    end
+
+    context 'a non-admin resident is signed in' do
+      before(:each) do
+        log_in_as @resident
+      end
+
+      it 'should raise an error' do
+        expect { delete :destroy, id: @group }.to raise_error Pundit::NotAuthorizedError
+      end
+
+      it 'should not be deleted' do
+        expect(Group.count).to eq 1
+      end
+    end
+
+    context 'a non-admin member of the group is signed in' do
+      before(:each) do
+        @resident.join @group
+        log_in_as @resident
+      end
+
       it 'should raise an error' do
         expect { delete :destroy, id: @group }.to raise_error Pundit::NotAuthorizedError
       end
