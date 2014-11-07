673a7f445c93baebbae6500a21f19ea2840b7291
Derek Maffett <derekgmaffett@gmail.com>
2014-11-07 07:07:47 -0800

    comment out avatar references in devise to avoid errors for now

--- app/assets/javascripts/ng_patient_tracker/app/groups/controllers/groups_ctrl.js
+++ app/assets/javascripts/ng_patient_tracker/app/groups/controllers/groups_ctrl.js
@@ -1,45 +1,47 @@
 (function() {
   var app = angular.module('CounterApp');
 
-  app.controller('GroupsCtrl', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
-    $scope.groups = {};
+  app.controller('GroupsCtrl', ['$scope', '$http', '$rootScope',
+    function($scope, $http, $rootScope) {
 
-    $scope.index = function() {
-      $http.get('api/v1/groups')
-        .success(function(data) {
-          $scope.groups = data['groups'];
-        })
-        .error(function(data, status) {
-          $scope.errors.push(data);
-          console.log(data);
-          console.log(status);
-        });
-    };
+      $scope.groups = {};
 
-    $scope.join = function(id, index) {
-      $http.post('api/v1/groups/' + id + '/join')
-        .success(function(data) {
-          $scope.groups = data['groups'];
-          $rootScope.setCurrentUserGroup(id);
-        })
-        .error(function(data, status) {
-          $scope.errors.push(data);
-          console.log(data);
-          console.log(status);
-        });
-    };
+      $scope.index = function() {
+        $http.get('api/v1/groups')
+          .success(function(data) {
+            $scope.groups = data['groups'];
+          })
+          .error(function(data, status) {
+            $scope.errors.push(data);
+            console.log(data);
+            console.log(status);
+          });
+      };
 
-    $scope.withdraw = function(id, index) {
-      $http.post('api/v1/groups/' + id + '/withdraw')
-        .success(function(data) {
-          $scope.groups = data['groups'];
-          $rootScope.setCurrentUserGroup(null);
-        })
-        .error(function(data, status) {
-          $scope.errors.push(data);
-          console.log(data);
-          console.log(status);
-        });
-    };
+      $scope.join = function(id, index) {
+        $http.post('api/v1/groups/' + id + '/join')
+          .success(function(data) {
+            $scope.groups = data['groups'];
+            $rootScope.setCurrentUserGroup(id);
+          })
+          .error(function(data, status) {
+            $scope.errors.push(data);
+            console.log(data);
+            console.log(status);
+          });
+      };
+
+      $scope.withdraw = function(id, index) {
+        $http.post('api/v1/groups/' + id + '/withdraw')
+          .success(function(data) {
+            $scope.groups = data['groups'];
+            $rootScope.setCurrentUserGroup(null);
+          })
+          .error(function(data, status) {
+            $scope.errors.push(data);
+            console.log(data);
+            console.log(status);
+          });
+      };
   }]);
 })();
